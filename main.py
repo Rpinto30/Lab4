e4d09fa8d3152484663f5f40f25fdc8458230710
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-05 09:53:15 -0600

    Ya sirve el cambio de frames

--- main.py
+++ main.py
@@ -6,11 +6,7 @@ def inscribir_banda():
     messagebox.showinfo("Inscripción", "Aquí se inscribiría una banda")
 
 def registrar_evaluacion():
-    global CURRENT_FRAME
-    #CURRENT_FRAME.pack_forget()
-    #register_frame.pack(expand=True, fill='both')
-    CURRENT_FRAME = register_frame
-    #messagebox.showinfo("Evaluación", "Aquí se registrarían los puntajes")
+    root.change_frame(register_frame)
 
 def listar_bandas():
     messagebox.showinfo("Listado", "Aquí se mostraría el listado de bandas")
@@ -22,36 +18,27 @@ def salir():
     root.quit()
 
 class Window(tk.Tk):
-    def __init__(self, frames:list[tk.Frame]):
+    def __init__(self, frame:tk.Frame):
         super().__init__()
         self.title("Concurso de Bandas - Quetzaltenango")
         self.geometry("800x500")
+        self.frames = frame
+        self.CURRENT_FRAME = frame
+        self.change_frame(self.CURRENT_FRAME)
 
-        self.frames = frames
-        CURRENT_FRAME = frames[0]
-        for F in frames: frame = F.master = self
+    def change_frame(self, frame):
+        self.CURRENT_FRAME = frame
+        self.CURRENT_FRAME.grid(row=0, column=0, sticky='nsew')
+        self.CURRENT_FRAME.tkraise()
 
-        CURRENT_FRAME.grid(row=0, column=0)
-
-    def change_frame(self, indx:int):
-        CURRENT_FRAME = self.frames[indx]
 BUTTONS_PRINCIPAL_MENU = []
-
-
 principal_frame = tk.Frame()
 principal_frame.config(bg='#4287f5', width=800, height=500)
 
 inscribe_frame = tk.Frame()
 
 register_frame = tk.Frame()
-register_frame.config(bg='#449147')
-
-root = Window([principal_frame])
-
-barra_menu = tk.Menu(root)
-
-#CURRENT_FRAME.pack(expand=True, fill='both')
-#principal_frame.grid(column=0, row=0, sticky='nsew', rowspan=10)
+register_frame.config(bg='#449147',width=1000, height=500)
 
 #-----------------------INSCRIBE------------------
 etiqueta = tk.Label(
@@ -62,7 +49,7 @@ etiqueta = tk.Label(
 )
 etiqueta.pack(pady=50)
 button_inscr_bad = tk.Button(principal_frame, text='Inscribir bandas por categoria')
-button_register = tk.Button(principal_frame, text='Registrar puntajes por criterios')
+button_register = tk.Button(principal_frame, text='Registrar puntajes por criterios', command=registrar_evaluacion)
 button_list_bands = tk.Button(principal_frame, text='Listar bandas inscritas')
 button_gen_rank = tk.Button(principal_frame, text='Generar ranking')
 BUTTONS_PRINCIPAL_MENU.extend([button_inscr_bad, button_register, button_list_bands, button_gen_rank])
@@ -72,18 +59,10 @@ for i in BUTTONS_PRINCIPAL_MENU:
 
 
 #------------------------REGISTER--------------
-
-lb = tk.Label(register_frame, text='Registro')
-
-menu_opciones = tk.Menu(barra_menu, tearoff=0)
-menu_opciones.add_command(label="Inscribir Banda", command=inscribir_banda)
-menu_opciones.add_command(label="Registrar Evaluación", command=registrar_evaluacion)
-menu_opciones.add_command(label="Listar Bandas", command=listar_bandas)
-menu_opciones.add_command(label="Ver Ranking", command=ver_ranking)
-menu_opciones.add_separator()
-menu_opciones.add_command(label="Salir", command=salir)
-
-barra_menu.add_cascade(label="Opciones", menu=menu_opciones)
-
-root.config(menu=barra_menu)
+lb = tk.Label(register_frame,
+    text="HOLAAA",
+    font=("Arial", 20, "bold"),
+    justify="center")
+lb.pack()
+root = Window(principal_frame)
 root.mainloop()
