98d7fc28c4f9ac539a4c02fdff7d620f1ad0709f
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-05 09:36:26 -0600

    Prueba de cambio de frames

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 def inscribir_banda():
     messagebox.showinfo("Inscripción", "Aquí se inscribiría una banda")
 
 def registrar_evaluacion():
-    messagebox.showinfo("Evaluación", "Aquí se registrarían los puntajes")
+    global CURRENT_FRAME
+    #CURRENT_FRAME.pack_forget()
+    #register_frame.pack(expand=True, fill='both')
+    CURRENT_FRAME = register_frame
+    #messagebox.showinfo("Evaluación", "Aquí se registrarían los puntajes")
 
 def listar_bandas():
     messagebox.showinfo("Listado", "Aquí se mostraría el listado de bandas")
@@ -16,19 +21,39 @@ def ver_ranking():
 def salir():
     root.quit()
 
+class Window(tk.Tk):
+    def __init__(self, frames:list[tk.Frame]):
+        super().__init__()
+        self.title("Concurso de Bandas - Quetzaltenango")
+        self.geometry("800x500")
+
+        self.frames = frames
+        CURRENT_FRAME = frames[0]
+        for F in frames: frame = F.master = self
+
+        CURRENT_FRAME.grid(row=0, column=0)
+
+    def change_frame(self, indx:int):
+        CURRENT_FRAME = self.frames[indx]
 BUTTONS_PRINCIPAL_MENU = []
 
-root = tk.Tk()
-root.title("Concurso de Bandas - Quetzaltenango")
-root.geometry("800x500")
+
+principal_frame = tk.Frame()
+principal_frame.config(bg='#4287f5', width=800, height=500)
+
+inscribe_frame = tk.Frame()
+
+register_frame = tk.Frame()
+register_frame.config(bg='#449147')
+
+root = Window([principal_frame])
 
 barra_menu = tk.Menu(root)
-principal_frame = tk.Frame(root)
-principal_frame.config(bg='#4287f5', width=800, height=500)
-principal_frame.pack(expand=True, fill='both')
+
+#CURRENT_FRAME.pack(expand=True, fill='both')
 #principal_frame.grid(column=0, row=0, sticky='nsew', rowspan=10)
 
-#BASE MENU PRINCIAPL
+#-----------------------INSCRIBE------------------
 etiqueta = tk.Label(
     principal_frame,
     text="Sistema de Inscripción y Evaluación de Bandas Escolares\nDesfile 15 de Septiembre - Quetzaltenango",
@@ -46,6 +71,10 @@ for i in BUTTONS_PRINCIPAL_MENU:
     i.pack(pady=10)
 
 
+#------------------------REGISTER--------------
+
+lb = tk.Label(register_frame, text='Registro')
+
 menu_opciones = tk.Menu(barra_menu, tearoff=0)
 menu_opciones.add_command(label="Inscribir Banda", command=inscribir_banda)
 menu_opciones.add_command(label="Registrar Evaluación", command=registrar_evaluacion)
@@ -57,7 +86,4 @@ menu_opciones.add_command(label="Salir", command=salir)
 barra_menu.add_cascade(label="Opciones", menu=menu_opciones)
 
 root.config(menu=barra_menu)
-
-
-
 root.mainloop()
