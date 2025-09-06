2e7d6ee0c4604771658dd5fe38b085f596304f17
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-05 18:36:54 -0600

    Pruebas en la creacion del formulario para registrar bandas YA ME ACOREDE COMO USAR TKINTER XD

--- main.py
+++ main.py
@@ -3,7 +3,35 @@ from tkinter import messagebox
 
 
 def inscribir_banda():
-    messagebox.showinfo("Inscripción", "Aquí se inscribiría una banda")
+    root.change_frame(inscribe_frame)
+    f_ib = tk.Frame(inscribe_frame)
+    f_ib.pack()
+    #inscribe_frame.grid_columnconfigure(0, weight=1)
+
+    l_ib_title = tk.Label(f_ib, text='Inscribir banda', font=('Arial',20))
+
+    #LABELS
+    l_ib_name = tk.Label(f_ib, text= 'Ingresa el nombre de tu banda')
+
+    l_ib_code = tk.Label(f_ib, text='Ingresa un codigo para tu banda')
+    l_ib_institution = tk.Label(f_ib, text='Ingresa un codigo para tu banda')
+    #ENTRIES
+    e_ib_name = tk.Entry(f_ib, width=25, font=('Arial', 15))
+    e_ib_code = tk.Entry(f_ib, width=25, font=('Arial', 15))
+    e_ib_institution = tk.Entry(f_ib, width=25, font=('Arial', 15))
+    e_ib_category = tk.Entry(f_ib, width=25, font=('Arial', 15))
+
+    b_ib_exit = tk.Button(f_ib, text='Cancelar', font=('Arial',12), command=return_main)
+
+
+    l_ib_title.grid(column=1, row=0, pady=20, sticky='nsew',columnspan=4)
+    l_ib_name.grid(column=1, row=1, padx=20)
+    e_ib_name.grid(column=2, row=1, padx=20)
+    l_ib_code.grid(column=1, row=2, padx=20, pady=10)
+    e_ib_code.grid(column=2, row=2, padx=20)
+    l_ib_code.grid(column=1, row=2, padx=20, pady=10)
+    e_ib_code.grid(column=2, row=2, padx=20)
+
 
 def registrar_evaluacion():
     root.change_frame(register_frame)
@@ -14,33 +42,41 @@ def listar_bandas():
 def ver_ranking():
     messagebox.showinfo("Ranking", "Aquí se mostraría el ranking final")
 
-def salir():
-    root.quit()
+
+def return_main(): root.change_frame(principal_frame)
+def salir(): root.quit()
 
 class Window(tk.Tk):
-    def __init__(self, frame:tk.Frame):
+    def __init__(self):
         super().__init__()
         self.title("Concurso de Bandas - Quetzaltenango")
         self.geometry("800x500")
-        self.frames = frame
-        self.CURRENT_FRAME = frame
-        self.change_frame(self.CURRENT_FRAME)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.CURRENT_FRAME = None
 
     def change_frame(self, frame):
+
         self.CURRENT_FRAME = frame
         self.CURRENT_FRAME.grid(row=0, column=0, sticky='nsew')
         self.CURRENT_FRAME.tkraise()
 
 BUTTONS_PRINCIPAL_MENU = []
-principal_frame = tk.Frame()
+
+root = Window()
+principal_frame = tk.Frame(root)
+root.change_frame(principal_frame)
+
 principal_frame.config(bg='#4287f5', width=800, height=500)
 
-inscribe_frame = tk.Frame()
+inscribe_frame = tk.Frame(root)
+inscribe_frame.config(bg='#ad3e3e',width=1000, height=500)
 
-register_frame = tk.Frame()
+register_frame = tk.Frame(root)
 register_frame.config(bg='#449147',width=1000, height=500)
 
-#-----------------------INSCRIBE------------------
+#-----------------------PRINCIPAL------------------
 etiqueta = tk.Label(
     principal_frame,
     text="Sistema de Inscripción y Evaluación de Bandas Escolares\nDesfile 15 de Septiembre - Quetzaltenango",
@@ -48,7 +84,7 @@ etiqueta = tk.Label(
     justify="center"
 )
 etiqueta.pack(pady=50)
-button_inscr_bad = tk.Button(principal_frame, text='Inscribir bandas por categoria')
+button_inscr_bad = tk.Button(principal_frame, text='Inscribir bandas por categoria', command=inscribir_banda)
 button_register = tk.Button(principal_frame, text='Registrar puntajes por criterios', command=registrar_evaluacion)
 button_list_bands = tk.Button(principal_frame, text='Listar bandas inscritas')
 button_gen_rank = tk.Button(principal_frame, text='Generar ranking')
@@ -58,11 +94,13 @@ for i in BUTTONS_PRINCIPAL_MENU:
     i.pack(pady=10)
 
 
-#------------------------REGISTER--------------
+#-----------------------INSCRIB BAND------------------
+
+
+#------------------------REGISTER NOTE--------------
 lb = tk.Label(register_frame,
     text="HOLAAA",
     font=("Arial", 20, "bold"),
     justify="center")
 lb.pack()
-root = Window(principal_frame)
 root.mainloop()
