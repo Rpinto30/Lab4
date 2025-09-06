c28609ef6937c0600758be9e2202ccb7f5e80a82
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-06 03:27:47 -0600

    Ahora se separa cada label y entry en filas para evitar usar grid y pack a la vez en dos frames

--- inscription_band.py
+++ inscription_band.py
@@ -1,31 +1,31 @@
 from usesful_funcs import *
 
 def insc_form(mainroot, inscription_frame, main_frame):
-
     mainroot.change_frame(inscription_frame)
 
-    f_ib_op = tk.Frame(inscription_frame) #PARA LAS OPCIONES
-    f_ib_acept = tk.Frame(inscription_frame, bg='red')
+    f_ib_op = tk.Frame(inscription_frame, width=550, height=350) #PARA LAS OPCIONES
+    f_ib_acept = tk.Frame(inscription_frame, width=550, height=150, bg='red')
 
-    f_ib_op.pack(expand=1, fill='y')
-    f_ib_acept.pack(expand=1, fill='y')
+    f_ib_op.pack_propagate(False)
+    f_ib_acept.pack_propagate(False)
+
+    f_ib_op.pack()
+    f_ib_acept.pack()
 
     l_ib_title = tk.Label(f_ib_op, text='Inscribir banda', font=('Arial',25))
-    pad_options = (20,40)
+    pad_options = (10,5)
 
     #LABELS
-    l_ib_name = tk.Label(f_ib_op, text= 'Ingresa el nombre de tu banda', anchor="w")
-    l_ib_code = tk.Label(f_ib_op, text='Ingresa un codigo para tu banda', anchor="w")
-    l_ib_institution = tk.Label(f_ib_op, text='Ingresa el nombre de la instutición para tu banda')
-    l_ib_category = tk.Label(f_ib_op, text='Ingresa una categoria para tu banda')
+    l_ib_name = tk.Label(text= 'Nombre de tu banda')
+    l_ib_code = tk.Label(text='Codigo para tu banda')
+    l_ib_institution = tk.Label(text='Instutición de tu banda')
+    l_ib_category = tk.Label(text='Categoria para tu banda')
     #ENTRY
-    e_ib_name = tk.Entry(f_ib_op, width=25, font=('Arial', 12), state='normal')
-    e_ib_code = tk.Entry(f_ib_op, width=25, font=('Arial', 12))
-    e_ib_code.delete(0, tk.END)
-    e_ib_institution = tk.Entry(f_ib_op, width=25, font=('Arial', 12))
-    e_ib_institution.delete(0, tk.END)
+    e_ib_name = tk.Entry(width=25, font=('Arial', 14), state='normal')
+    e_ib_code = tk.Entry(width=10, font=('Arial', 14))
+    e_ib_institution = tk.Entry(width=10, font=('Arial', 14))
     #COMBOBOX
-    c_ib_category = Combobox(f_ib_op, width=23, state='readonly', font=('Arial', 12),
+    c_ib_category = Combobox(width=17, state='readonly', font=('Arial', 14),
                              values=['Primaria','Básico', 'Bachillerato'])
     c_ib_category.current(0) #PRIMARIA POR DEFECTO
 
@@ -45,7 +45,7 @@ def insc_form(mainroot, inscription_frame, main_frame):
                 l_ib_info.config(text=valid)
         else:
             disable_widgets([e_ib_name, e_ib_institution, e_ib_code, c_ib_category, b_ib_acept, b_ib_cancel])
-            mainroot.after(ms=2500, func=exit_form)
+            mainroot.after(ms=1200, func=exit_form)
     def exit_form():
         b_ib_acept.config(text='Aceptar')
         b_ib_cancel.config(text='Cancelar')
@@ -62,25 +62,23 @@ def insc_form(mainroot, inscription_frame, main_frame):
                            command=validar_inscripcion)
     b_ib_cancel = tk.Button(f_ib_acept, text='Cancelar', font=('Arial', 12), width=15,
                            command=verify_cancel)
-    # GRID
-    l_ib_title.grid(column=1, row=0, sticky='nsew', columnspan=4, pady=40)
-    l_ib_name.grid(column=1, row=1, padx=pad_options[0])
-    e_ib_name.grid(column=2, row=1, padx=pad_options[0])
-    l_ib_code.grid(column=1, row=2, padx=pad_options[0], pady=pad_options[1])
-    e_ib_code.grid(column=2, row=2, padx=pad_options[0])
-    l_ib_category.grid(column=1, row=3, padx=pad_options[0], pady=pad_options[1] - pad_options[1] // 1.1)
-    c_ib_category.grid(column=2, row=3, padx=pad_options[0])
-    l_ib_institution.grid(column=1, row=4, padx=pad_options[0], pady=pad_options[1])
-    e_ib_institution.grid(column=2, row=4, padx=pad_options[0])
+    # PACK
+    l_ib_title.pack(pady=40)
+    pack_create_line(f_ib_op,l_ib_name, e_ib_name, pad_options[0], pad_options[1], 460,30) #NOMBRE
+    pack_create_line(f_ib_op,l_ib_code, e_ib_code, pad_options[0], pad_options[1], 460,30) #CODIGO
+    pack_create_line(f_ib_op,l_ib_institution, e_ib_institution, pad_options[0], pad_options[1], 460,30) #INSTITUCION
+    pack_create_line(f_ib_op,l_ib_category, c_ib_category, pad_options[0], pad_options[1], 460,30) #CATEGORIA
 
-    l_ib_info.grid(column=1, row=5, columnspan=2, pady=5)
+    #l_ib_code.pack(padx=pad_options[0], pady=pad_options[1], side='left')
+    #e_ib_code.pack(padx=pad_options[0]//3, side='right')
+    #l_ib_category.pack(padx=pad_options[0], pady=pad_options[1] - pad_options[1] // 1.1, side='left')
+    #c_ib_category.pack(padx=pad_options[0]//3, side='right')
+    #l_ib_institution.pack(padx=pad_options[0], pady=pad_options[1], side='left')
+    #e_ib_institution.pack(padx=pad_options[0]//3)
 
-    #Encontré esta solución de evitar el frame no cambie con pack_propagate y usar update_idletast para que se calcule
-    #el tamaño del frame anterior y se aplique al otro frame
-    mainroot.update_idletasks()
-    print(l_ib_title.winfo_width())
-    f_ib_acept.config(width=f_ib_op.winfo_width())
-    f_ib_acept.pack_propagate(False)
-    b_ib_acept.pack(side='left', padx=100)
+    l_ib_info.pack(pady=5)
+
+    b_ib_acept.pack(side='left')
     b_ib_cancel.pack(side='left')
+
 
--- usesful_funcs.py
+++ usesful_funcs.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 from tkinter.ttk import Combobox
 
+
+def pack_create_line(master:tk.Frame, left_widget, right_widget, _padx=0, _pady=0, width=0,height=0):
+    row = tk.Frame(master,bg='green', width=width+5, height=height)
+    left_widget.pack(in_=row, side='left', anchor='center', padx=_padx)
+    right_widget.pack(in_=row, side='right', anchor='center', padx=_padx)
+    row.pack_propagate(False)
+    row.pack(pady=_pady)
+    return row
+
+
 def clear_widgets(widgets:list):
     for widget in widgets:
         if isinstance(widget, tk.Entry): widget.delete(0, tk.END)
