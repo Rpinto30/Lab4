71354e5fceaa3dc77ce8feaced0c0a034aa79e46
rodri <rodrihercar24@gmail.com>
2025-09-05 22:52:57 -0600

    creacio de clases

--- Clases.py
+++ Clases.py
@@ -4,55 +4,57 @@ import tkinter as tk
 class BandaParticipante:
     def __init__(self, nombre, institucion, codigo):
         self.nombre = nombre
-        self.institucion = institucion
-        self.codigo = codigo
+        self._institucion = institucion
+        self._codigo = codigo
 
     def mostrar_info(self):
-        return f"Nombre de la banda {self.nombre} | Institucion {self.institucion} | Codigo {self.codigo}"
+        return f"Nombre de la banda {self.nombre} | Institucion {self._institucion} | Codigo {self._codigo}"
 
 categorias = ["primaria","basico", "bachillerato"]
 class BandaEscolar(BandaParticipante):
-    def __init__(self, nombre, institucion, codigo, categoria, puntajes):
+    def __init__(self, nombre, institucion, codigo, categoria, puntaje):
         super().__init__(nombre, institucion, codigo)
         self._categoria = categoria
-        self._puntajes = {}
+        self._puntaje = 0
 
-    def get_categoria(self):
+    @property
+    def categoria(self):
         return categorias
 
     def set_categoria(self, categori):
-        if self._categoria in categorias:
+        if categori in categorias:
             self._categoria = categori
-            return  True
-        else:
-            return "Debe ser una de las categorias indicadas"
 
     def registrar_puntajes(self, puntos):
-        if puntos >= 0 and puntos <= 10:
-            self._puntajes = puntos
-            return True
-        else:
-            return "Debe ser uno de los puntajes dentro del rango"
+        if  0 <= puntos <= 10:
+            self._puntaje = puntos
 
     def mostrar_info(self):
-        return f"Categoria {self._categoria} | Puntaje {self._puntajes}"
+        pass
 
-be = BandaEscolar
 class Concurso:
     def __init__(self):
         self.bandas = {}
 
-    def inscribir_banda(self, entry_nombre:tk.Entry, entry_institucion:tk.Entry, entry_codigo:tk.Entry ):
-        while True:
-            entry_codigo = tk.Entry(entry_codigo)
-            if entry_codigo not in self.bandas:
-                break
-            else:
-                pass
+    def inscribir_banda(self, entry_nombre:str, entry_institucion:str, entry_codigo:str, entry_categoria:str ):
 
-        entry_nombre = tk.Entry(entry_nombre)
-        entry_institucion = tk.Entry(entry_institucion)
-        self.bandas[entry_codigo.get()] = BandaEscolar
+        if entry_codigo not in self.bandas:
+            if entry_nombre.strip() != "":
+                if entry_institucion.strip() != "":
+                    if entry_categoria.strip() != "":
+                        self.bandas[entry_codigo] = BandaEscolar(entry_nombre, entry_institucion, entry_codigo, entry_categoria, 0)
+                        return 0
+                    else:
+                        return "Categoria vacia"
+                else:
+                    return "Institucion vacia"
+            else:
+                return "Nombre vacio"
+        else:
+            return "Codigo de banda ya existente"
+
+
+
 
     def registrar_puntaje(self, search_codigo:tk.Entry, entry_puntaje:tk.Entry):
         search_codigo = tk.Entry(search_codigo)
@@ -60,9 +62,8 @@ class Concurso:
             pass
 
     def listar_bandas(self):
-        for codigo, valor in self.bandas.items():
-            return (f"Codigo {codigo} | Nombre {valor.entry_nombre} | puntajes {valor.puntajes}")
-
+        for banda in self.bandas.values():
+            banda.mostrar_info()
 
 
 
