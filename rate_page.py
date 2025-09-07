bf433933caee69e497054173bfe22ba63547899c
Rpinto30 <rodrigopinto3007@gmail.com>
2025-09-07 00:08:27 -0600

    Agrego un combobox para elegir la vanda que se desea evaluar en la ocpion de rate banda

--- rate_page.py
+++ rate_page.py
@@ -3,9 +3,13 @@ from tkinter.ttk import Combobox
 
 def rate_form(mainroot, rate_frame, main_frame):
     mainroot.change_frame(rate_frame)
-    f_combo_select = tk.Frame(rate_frame, width=500, height=100)
+    f_combo_select = tk.Frame(rate_frame, width=500, height=75)
     f_combo_select.pack_propagate(False)
-    f_combo_select.pack()
+    f_combo_select.pack(anchor='center', fill='x')
 
-    c_list_bands = Combobox(f_combo_select, state='readonly',values=['a','b'])
-    c_list_bands.pack()
+    bands = [band for band in mainroot.concurso.bandas.values()]
+    l_header = tk.Label(f_combo_select, text= 'Calificar Banda', font=('Arial',20))
+    l_header.pack()
+    c_list_bands = Combobox(f_combo_select, state='readonly',values=[b.nombre for b in bands], width=30, font=('Arial', 15))
+    c_list_bands.pack(expand=True, anchor="center")
+
